070a89022ea976704f7959c672ade9fcda660743
Tatana <burmatova@inbox.ru>
2020-09-04 19:45:59 +0300

    Added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+public class PostManager {
+
+    private PostManager[] posts;
+
+    public PostManager[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+
+//        TODO: add logic
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+//        TODO: add logic
+    }
+}
